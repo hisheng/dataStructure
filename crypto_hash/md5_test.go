bc8e1b337b8768b586d7b99e079e00e86e5fea7d
张海生 <zhanghaisheng@km.com>
2020-05-09 12:00:25 +0800

    dm5

---
+++ crypto_hash/md5_test.go
@@ -0,0 +1,24 @@
+package crypto_hash
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"testing"
+)
+
+func TestMd5(t *testing.T) {
+	//方法1
+	m5 := md5.New()
+	io.WriteString(m5, "mynameishishneg")
+	m5s := fmt.Sprintf("%x", m5.Sum(nil))
+	t.Log(m5s)
+
+	m5 = md5.New()
+	io.WriteString(m5, "hiword")
+	m5s = fmt.Sprintf("%x", m5.Sum(nil))
+	t.Log(m5s)
+	//方法2
+	t.Log(fmt.Sprintf("%x", md5.Sum([]byte("mynameishishneg"))))
+	t.Log(fmt.Sprintf("%x", md5.Sum([]byte("hiword"))))
+}
