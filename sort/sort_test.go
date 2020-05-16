dd85201ebb333826ea69590eb2ddb1156b5f2790
张海生 <zhanghaisheng@km.com>
2020-05-16 17:28:00 +0800

    Multiple sort

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -46,3 +46,40 @@ func TestSort(t *testing.T) {
 	//张海生 5-14 09:12:46
 	//快速排序 ，堆排序 不是稳定的排序算法
 }
+
+type PersonMultipleSlice []PersonMultiple
+type PersonMultiple struct {
+	Name   string
+	Age    int
+	Height int
+}
+
+func (ps PersonMultipleSlice) Len() int      { return len(ps) }
+func (ps PersonMultipleSlice) Swap(i, j int) { ps[i], ps[j] = ps[j], ps[i] }
+func (ps PersonMultipleSlice) Less(i, j int) bool {
+	if ps[i].Age < ps[j].Age {
+		return true
+	}
+	if ps[i].Age > ps[j].Age {
+		return false
+	}
+
+	return ps[i].Height < ps[j].Height
+}
+
+//多阶排序 (此时是需要稳定排序的)
+//https://stackoverflow.com/questions/36122668/how-to-sort-struct-with-multiple-sort-parameters
+func TestMultiple(t *testing.T) {
+	//先按age 升序，然后 再按身高 升序
+	ps := []PersonMultiple{
+		{"bo", 31, 11},
+		{"ao", 42, 20},
+		{"ao", 42, 18},
+		{"ao", 42, 12},
+		{"ao", 42, 21},
+		{"co", 17, 13},
+		{"do", 26, 14},
+	}
+	sort.Sort(PersonMultipleSlice(ps))
+	t.Log(ps)
+}
