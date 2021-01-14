9e91616326c82581916bbace77f1e63ed7ad8a6e
张海生 <zhanghaisheng@qimao.com>
2021-01-14 20:06:52 +0800

    map

--- map/map_test.go
+++ map/map_test.go
@@ -54,3 +54,71 @@ func TestMapKey(t *testing.T) {
 	t.Log("----end")
 
 }
+
+func TestMapInit(t *testing.T) {
+	m1 := map[int]int{1: 1, 2: 2, 3: 3}
+	t.Log(m1[2])
+	t.Logf("lem m1 = %d", len(m1))
+
+	m3 := make(map[int]int, 10) // 10 是容量，len()是0
+	t.Log(m3)                   //map[] 用len() 函数判断为空 不能用nil
+	if m3 != nil {
+		t.Log("is not nil")
+	}
+
+	m4 := map[int]int{} //map[]
+	if m4 != nil {
+		t.Log("m4 is not nil")
+	}
+	// m4[5]  不存在会返回0
+	//需要
+	if v, ok := m4[5]; ok {
+		t.Log(v)
+	} else {
+		t.Log("no ")
+	}
+
+	for k, v := range m1 {
+		t.Log(k, v)
+	}
+	//打印的是乱序
+
+}
+
+func TestMapWithValue(t *testing.T) {
+	m := map[int]func(op int) int{}
+	m[1] = func(op int) int {
+		return op
+	}
+	m[2] = func(op int) int {
+		return op * op
+	}
+	m[3] = func(op int) int {
+		return op * op * op
+	}
+	t.Log(m[1](2), m[2](2), m[3](2)) //2 4 8
+
+	n := m
+	delete(m, 1)
+	t.Log(len(m)) //2
+	t.Log(len(n)) //2
+
+}
+
+func TestMapForSet(t *testing.T) {
+	set := map[int]bool{}
+	set[1] = true
+	if set[1] {
+		t.Log("ok")
+	} else {
+		t.Log("no")
+	}
+	t.Log(set[3]) //false
+
+	m := map[int]string{}
+	t.Log(m[3]) //空字符串
+
+	delete(set, 1)
+	t.Log(len(set)) //变成0了
+
+}
