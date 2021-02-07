911b3ff9bb192a10e028b3b173c152e0074c9fb3
张海生 <zhanghaisheng@qimao.com>
2021-02-07 16:10:01 +0800

    测试多维数组

--- array/array_test.go
+++ array/array_test.go
@@ -16,6 +16,22 @@ func TestArray(t *testing.T) {
 	}(x)
 
 	t.Log(x) //prints [1 2 3] (not ok if you need [7 2 3])
-
 	//如果是 slice 的话，会一样，array是不一样的
+}
+
+/**
+ * 测试多维数组
+ *
+ * @author zhanghaisheng@qimao.com
+ * @dateTime   : 2021/2/7 4:09 下午
+ */
+func TestArrayArray(t *testing.T) {
+	x := 2
+	y := 4
+
+	table := make([][]int, x)
+	for i := range table {
+		table[i] = make([]int, y)
+	}
+	t.Log(table) //[[0 0 0 0] [0 0 0 0]]
 }
