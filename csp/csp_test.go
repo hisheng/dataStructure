6fd1324e4f5c1d8dc7920ea058109d80197e42f0
张海生 <zhanghaisheng@qimao.com>
2021-03-19 10:10:07 +0800

    channel

--- csp/csp_test.go
+++ csp/csp_test.go
@@ -4,3 +4,25 @@
 @Desc
 */
 package csp
+
+import "testing"
+
+//1.关闭一个 nil 值 channel 会引发 panic。！
+func TestNew(t *testing.T) {
+	var ch chan struct{}
+	close(ch)
+}
+
+//2. 关闭一个已关闭的 channel 会引发 panic。
+func TestClose(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+	close(ch)
+}
+
+//3. 向一个已关闭的 channel 发送数据。会引发 panic。
+func TestCloseSend(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+	ch <- struct{}{}
+}
